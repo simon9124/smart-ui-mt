e125805877693c5ffb7514a184ecca3519ebabfb
Simon9124 <435776313@qq.com>
2018-07-09 17:53:30 +0800

    遮罩层

--- src/views/layout/components/leftMenu.vue
+++ src/views/layout/components/leftMenu.vue
@@ -1,20 +1,32 @@
 <template>
   <div class="header-slot">
+    <!-- 遮罩层 -->
+    <div v-show="show"
+         class='header-slot-mask'
+         @click='show=!show'
+         ref='cover'></div>
+    <!-- 面包屑按钮 -->
     <mt-button slot="right"
                @click="show=!show">
       <i class="icon iconfont icon-menu"></i>
     </mt-button>
+    <!-- 菜单栏 -->
     <transition name="fade">
       <div>
         <!-- <h3>SmartBCM 业务连续性管理系统</h3> -->
         <ul class="header-slot-leftmenu"
             v-show="show">
-          <li class="header-slot-leftmenu-item">系统设置</li>
-          <li class="header-slot-leftmenu-item">用户管理</li>
-          <li class="header-slot-leftmenu-item">树形结构</li>
-          <li class="header-slot-leftmenu-item">智能图表</li>
-          <li class="header-slot-leftmenu-item">功能组件</li>
-          <li class="header-slot-leftmenu-item">动态皮肤</li>
+          <a class="header-slot-leftmenu-item"
+             href='#/'>首页</a>
+          <a class="header-slot-leftmenu-item"
+             href='#/settings'>系统设置</a>
+          <a class="header-slot-leftmenu-item"
+             href='#/userManager'>用户管理</a>
+          <a class="header-slot-leftmenu-item"
+             href='#/tree'>树形结构</a>
+          <a class="header-slot-leftmenu-item">智能图表</a>
+          <a class="header-slot-leftmenu-item">功能组件</a>
+          <a class="header-slot-leftmenu-item">动态皮肤</a>
         </ul>
       </div>
     </transition>
@@ -41,6 +53,16 @@ export default {
 <style rel="stylesheet/scss" lang="scss">
 .header-slot {
   position: relative;
+  &-mask {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background: #2c3b41;
+    opacity: 0.5;
+    z-index: 1111;
+  }
   &-leftmenu {
     position: absolute;
     top: 40px;
@@ -49,7 +71,10 @@ export default {
     margin: 0;
     padding: 0;
     &-item {
-      list-style: none;
+      display: block;
+      color: #fff;
+      text-decoration: none;
+      // list-style: none;
       padding: 10px 30px 10px 15px;
       &:not(:first-child) {
         border-top: 1px solid #fff;
